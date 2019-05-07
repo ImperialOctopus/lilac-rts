aa96c8b069df0624742571c54edd422582edfc8e
Zoey Child <imperialoctopus@gmail.com>
2019-05-07 01:23:06 +0100

    Install pixi dart

--- web/scripts/game.dart
+++ web/scripts/game.dart
@@ -1,57 +1,21 @@
 import 'dart:html';
 
-import 'package:stagexl/stagexl.dart';
 import 'package:vector_math/vector_math.dart';
-import 'keyboard.dart';
-import 'lavendar/engine.dart';
-import 'lavendar/entities/entity.dart';
-import 'lavendar/time.dart';
-import 'renderer/menu/menu.dart';
-import 'renderer/renderer.dart';
-import 'unit_select.dart';
 
-const int stageHeight = 600;
-const int stageWidth = 600;
-const int backgroundColor = Color.White;
-const int foregroundColor = Color.CornflowerBlue;
+import 'input/keyboard.dart';
+import 'lilac/engine.dart';
+import 'lilac/entities/entity.dart';
+import 'lilac/time.dart';
 
 class Game {
-  static double deltaTime;
+  Engine engine;
+  Keyboard keyboard;
 
-  static Stage stage;
-  static ResourceManager resourceManager;
-
-  static Menu menu;
-  static Engine engine;
-  static Renderer renderer;
-  static UnitSelect unitSelect;
-  static Keyboard keyboard;
-
-  Game(_stage, _resourceManager) {
-    stage = _stage;
-    resourceManager = _resourceManager;
-  }
-
-  void load() {
-    renderer = new Renderer();
-    keyboard = new Keyboard();
-
-    resourceManager
-      ..addBitmapData('menu_play', 'assets/menu/play.png')
-      ..addBitmapData('ui_move', 'assets/ui/move.png')
-      ..addBitmapData('ui_attack', 'assets/ui/attack.png');
-    resourceManager.load().then((r) => {startMenu()});
-  }
-
-  void startMenu() {
-    menu = new Menu();
-  }
+  Game() {}
 
   void startGame() {
+    keyboard = new Keyboard();
     engine = new Engine();
-    unitSelect = new UnitSelect();
-
-    stage.onEnterFrame.listen(update);
     engine.stream.streamController.stream.listen((e) => entityCreated(e));
 
     keyboard.addBinding(KeyCode.P, () => {Time.pause()});
@@ -68,10 +32,8 @@ class Game {
     engine.createUnit(new Vector2(550, 550), Team.Enemy);
   }
 
-  void update(EnterFrameEvent f) {
-    deltaTime = f.passedTime;
+  void update() {
     engine.update();
-    renderer.update();
   }
 
   void entityCreated(CustomEvent event) {
