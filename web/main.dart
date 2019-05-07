d855815b8b3aab52597f38fef40983cfe2d6c7d2
Zoey Child <imperialoctopus@gmail.com>
2019-05-07 22:40:40 +0100

    Add pixi test code

--- web/main.dart
+++ web/main.dart
@@ -1,9 +1,18 @@
 import 'dart:html' hide Point;
 import 'package:pixi/pixi.dart';
 
+Loader loader;
+
 main() {
   Application app = new Application(
       new Options(height: 256, width: 256, backgroundColor: 0x061639));
-
   document.body.append(app.view);
+
+  Sprite sprite;
+
+  loader = new Loader();
+  loader
+    ..add('menu_play', "assets/menu/play.png")
+    ..load((loader, resources) =>
+        {sprite = new Sprite(resources.menu_play.texture)});
 }
